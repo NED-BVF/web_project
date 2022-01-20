aefca61ba2fa786a4ee94c1419564fa1b3cb811b
BluWeBlood <blueweblood@gmail.com>
2022-01-20 12:33:57 +0900

    Column 오류 수정

--- fanCM/fanCM/src/main/java/com/jsc/fanCM/domain/Article.java
+++ fanCM/fanCM/src/main/java/com/jsc/fanCM/domain/Article.java
@@ -10,18 +10,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
     @Id
-    @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
     private String body;
-    private LocalDateTime regDate = LocalDateTime.now();
-    private LocalDateTime updateDate = LocalDateTime.now();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    private LocalDateTime regDate = LocalDateTime.now();
+    private LocalDateTime updateDate = LocalDateTime.now();
+
     //생성 매소드
     public static Article createArticle(String title,String body) {
         Article article = new Article();
